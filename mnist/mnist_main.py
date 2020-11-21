0bf384ac8bf81faf32d78233795baf3a256b3c09
memo <mahmood.ghz@gmail.com>
2020-11-21 17:34:29 -0500

    try knn for mnist database which take long time and had no result-not appropriate!

--- mnist/mnist_main.py
+++ mnist/mnist_main.py
@@ -1,20 +1,30 @@
 import pandas as pd     
 import numpy as np 
 from tensorflow.keras.datasets.mnist import load_data
+import matplotlib.pyplot as plt 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import time
 
 # (x_train,y_train),(x_test,y_test) = load_data(path='mnist.npz')
-
-# data = load_data(path='mnist.npz') // downloading for testing 
-# np.savez_compressed('mahmood.npz', data)
+# data = load_data(path='mnist.npz');np.savez_compressed('mahmood.npz', data) // downloading for testing 
 (x_train,y_train),(x_test,y_test) = load_data(path='mahmood.npz')
-print(x_train)
+print(x_train.shape)
+x_train = x_train/255
+x_test = x_test/255
+x_train = np.reshape(x_train,(60000,784))
+x_test = np.reshape(x_train,(60000,784))
+print(x_train.shape)
 
+t1 = time.process_time()
 
-
-
-
-
-
+knn = KNeighborsClassifier(n_neighbors=10)
+knn.fit(x_train,y_train)
+prediction = knn.predict(x_test)
+accuracy = accuracy_score(prediction,y_test)
+print(accuracy)
+t2 = time.process_time()
+print(t2-t1)
 
 
 
