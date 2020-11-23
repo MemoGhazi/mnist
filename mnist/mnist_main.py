a957a697105edfc7664250951a3e7f57cfdf6a6e
memo <mahmood.ghz@gmail.com>
2020-11-23 05:32:45 -0500

    svm done

--- mnist/mnist_main.py
+++ mnist/mnist_main.py
@@ -3,6 +3,7 @@ import numpy as np
 from tensorflow.keras.datasets.mnist import load_data
 import matplotlib.pyplot as plt 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn import svm
 from sklearn.metrics import accuracy_score
 import time
 
@@ -16,18 +17,24 @@ x_train = np.reshape(x_train,(60000,784))
 x_test = np.reshape(x_train,(60000,784))
 print(x_train.shape)
 
-t1 = time.process_time()
 
-knn = KNeighborsClassifier(n_neighbors=10)
-knn.fit(x_train,y_train)
-prediction = knn.predict(x_test)
+# t1 = time.process_time()
+# knn = KNeighborsClassifier(n_neighbors=10)
+# knn.fit(x_train,y_train)
+# prediction = knn.predict(x_test)
+# accuracy = accuracy_score(prediction,y_test)
+# print(accuracy)
+# t2 = time.process_time()
+# print(t2-t1)
+
+
+t1 = time.process_time()
+sv = svm.SVC()
+sv.fit(x_train,y_train)
+prediction = sv.predict(x_test)
 accuracy = accuracy_score(prediction,y_test)
-print(accuracy)
 t2 = time.process_time()
 print(t2-t1)
-
-
-
 
 
 
